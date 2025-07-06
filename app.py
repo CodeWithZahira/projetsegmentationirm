eef8d28e6977cee5cf0cfac2af011aad9cc35d70
CodeWithZahira <z.ellaouah9465@uca.ac.ma>
2025-07-06 19:10:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,7 @@ MODEL_DRIVE_ID = "1ti_-AtZzOHTVzHAHnQJyJuNzfu4TD7IB"
 MODEL_PATH = "unet_model_finetuned.h5"
 IMG_SIZE = (128, 128)
 
-# -------- Custom dice functions --------
-def dice_coef(y_true, y_pred, smooth=1):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
-def dice_loss(y_true, y_pred):
-    return 1 - dice_coef(y_true, y_pred)
 
 # -------- Download model from Drive --------
 def download_model():
