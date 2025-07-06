b25b2f1d058e3ba58fa932ebfa2afcbf961dbff5
CodeWithZahira <z.ellaouah9465@uca.ac.ma>
2025-07-06 23:54:33 +0000

    Add files via upload

---
+++ pages/About.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("📖 About NeuroSeg")
+
+st.markdown("""
+**NeuroSeg** is a deep learning project for automatic segmentation of brain MRI images.
+
+- Built with U-Net architecture
+- Trained on a custom dataset
+- Supports TFLite for lightweight prediction
+
+_Developed by Zahira Ellaouah._
+""")
