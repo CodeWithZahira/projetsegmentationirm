8608429324eda51821a7d4044cd850619887c561
CodeWithZahira <z.ellaouah9465@uca.ac.ma>
2025-07-07 19:30:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def set_bg_from_url(image_url):
         </style>
     """, unsafe_allow_html=True)
 
-bg_image = "https://as1.ftcdn.net/v2/jpg/06/09/64/42/1000_F_609644210_RSe84rM4Ad1ZAwZ5MvJqh3VCdb0L3wOx.jpg"
+bg_image = "https://i.pinimg.com/originals/a2/33/e8/a233e808eb74686d9544b3960b0f1eb9.jpg"
 set_bg_from_url(bg_image)
 
 # =============================
