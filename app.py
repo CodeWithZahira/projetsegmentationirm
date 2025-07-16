46812340f77a1d7cd457c86fd95803588f004714
CodeWithZahira <z.ellaouah9465@uca.ac.ma>
2025-07-16 13:50:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(page_title="NeuroSeg Advanced", layout="wide")
 # =============================
 # I've replaced the local file logic with a direct URL.
 # Replace this URL with the one you copied from Google or another image source.
-image_url = "https://png.pngtree.com/background/20230617/original/pngtree-3d-rendered-brain-illustration-picture-image_3696609.jpg" 
+image_url = "https://img.freepik.com/premium-photo/pink-background-reveals-intricate-3d-rendering-human-brains-captivating-anatomical-model-patte_1000124-213206.jpg" 
 
 page_bg_img = f"""
 <style>
