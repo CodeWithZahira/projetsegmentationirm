e5198042d5033ea70d2c192da88704a9eb72e242
CodeWithZahira <z.ellaouah9465@uca.ac.ma>
2025-07-16 13:07:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import streamlit.components.v1 as com
 # =============================
 # 🔧 PAGE CONFIG
 # =============================
-st.set_page_config(page_title="NeuroSeg Base")  
+st.set_page_config(page_title="NeuroSeg Base", color = black)  
 
 # =============================
 # 💬 Bienvenue + Animation
